4d12fe4c077205f265453f5f5497b5125f493eb7
a-mackay <amk@gegroup.com.au>
2019-10-31 18:19:28 +1100

    Fix doc comment formatting

--- src/lib.rs
+++ src/lib.rs
@@ -5,26 +5,28 @@
 //! (watch* operations, pointWrite and invokeAction).
 //!
 //! # Example Usage
-//! 1. Put this in the main function in the `main.rs` file to create
+//! Put this in the main function in the `main.rs` file to create
 //! and use a `SkySparkClient`:
-//!     ```rust,no_run
-//!     # async fn run() {
-//!     use raystack::{SkySparkClient, ValueExt};
-//!     use ring::rand::SystemRandom;
-//!     use url::Url;
-//!     let rng = SystemRandom::new();
-//!     let url = Url::parse("https://www.example.com/api/projName/").unwrap();
-//!     let client = SkySparkClient::new(url, "username", "p4ssw0rd", None, &rng).await.unwrap();
-//!     let sites_grid = client.eval("readAll(site)").await.unwrap();
-//!     
-//!     // Print the raw JSON:
-//!     println!("{}", sites_grid.to_json_string_pretty());
-//!     
-//!     // Working with the Grid struct:
-//!     println!("All columns: {:?}", sites_grid.cols());
-//!     println!("first site id: {:?}", sites_grid.rows()[0]["id"].as_hs_ref().unwrap());
-//!     # }
-//!     ```
+//! 
+//! ```rust,no_run
+//! # async fn run() {
+//! use raystack::{SkySparkClient, ValueExt};
+//! use ring::rand::SystemRandom;
+//! use url::Url;
+//! 
+//! let rng = SystemRandom::new();
+//! let url = Url::parse("https://www.example.com/api/projName/").unwrap();
+//! let client = SkySparkClient::new(url, "username", "p4ssw0rd", None, &rng).await.unwrap();
+//! let sites_grid = client.eval("readAll(site)").await.unwrap();
+//! 
+//! // Print the raw JSON:
+//! println!("{}", sites_grid.to_json_string_pretty());
+//! 
+//! // Working with the Grid struct:
+//! println!("All columns: {:?}", sites_grid.cols());
+//! println!("first site id: {:?}", sites_grid.rows()[0]["id"].as_hs_ref().unwrap());
+//! # }
+//! ```
 //! 
 //! See the `examples` folder for more usage examples.
 //!
